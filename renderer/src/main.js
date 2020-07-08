b500da437c9993cbd8f113a05ebaa20150c296b5
unknown <auronwalkeris@gmail.com>
2020-07-08 17:27:12 +1200

    Added two types of grid generations

--- renderer/src/main.js
+++ renderer/src/main.js
@@ -1,2 +1,13 @@
-const icons = document.querySelectorAll(".icon");
-console.log(icons);
+const mapX = 0;
+const mapY = 0;
+
+function createGrid(){
+    const map = document.querySelector(".map");
+    if(map === null) return;
+    for (let index = 0; index < 135; index++) {
+        const grid = document.createElement("div");
+        grid.classList.add("grid");
+        map.appendChild(grid);
+    }
+}
+createGrid();
