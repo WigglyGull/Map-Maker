eac0ffc09e9b1fdb510cd4bafe34825bae1db163
unknown <auronwalkeris@gmail.com>
2020-08-01 16:50:09 +1200

    started working on room placement

--- renderer/src/grid.js
+++ renderer/src/grid.js
@@ -1,13 +1,36 @@
 const mapRows = 16;
 const mapColumns = 10;
 
-exports.createGrid = map => {
-    if(map === null) return;
-    const amountofSqaures = mapRows * mapColumns;
+exports.gridList = [];
 
+exports.createGrid = gridHolder => {
+    if(gridHolder === null) return;
+    const amountofSqaures = mapRows * mapColumns;
+    const halfAmount = amountofSqaures/2 + 8;
+
+    //Spawns the grid
     for (let index = 0; index < amountofSqaures; index++) {
         const grid = document.createElement("div");
         grid.classList.add("grid");
-        map.appendChild(grid);
+        grid.classList.add(`${index}`);
+        
+        if(halfAmount === index){
+            grid.classList.add("check");
+        }
+        gridHolder.appendChild(grid);
     }
+
+    //Adds interactity
+    this.gridList = document.querySelectorAll(".grid");
+    this.gridList.forEach(grid => {
+        grid.addEventListener("click", ()=>{
+            console.log("help for the love of all that is holy");
+        });
+    });
+}
+
+function createRoom(grid){
+    const room = document.createElement("div");
+    room.classList.add("room");
+    grid.appendChild(room);
 }
--- renderer/src/main.js
+++ renderer/src/main.js
@@ -3,11 +3,15 @@ const tool = require("./src/tool.js");
 const biome = require("./src/biome.js");
 
 const map = document.querySelector(".map");
+const gridHolder = document.querySelector(".gridHolder");
 const toolBar = document.querySelector(".toolBar");
 const biomeBar = document.querySelector(".biomeBar");
 let tools = document.querySelectorAll(".tool");
 let biomes = document.querySelectorAll(".biome");
 
-grid.createGrid(map);
+grid.createGrid(gridHolder);
 tool.createToolbar(tools);
 biome.createBiome(biomes);
+
+console.log(tools);
+console.log(grid.gridList);
